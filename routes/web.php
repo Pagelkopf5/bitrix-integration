8e885f1b52472c493935374eab5f160020647bbd
Gabriel Pagelkopf <gabriel.pagelkopf@gmail.com>
2025-01-05 23:04:38 -0300

    add get route

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller;
 
 Route::post('/', [Controller::class, 'verify']);
+Route::get('/', function () {
+    return 'Hello World';
+});
